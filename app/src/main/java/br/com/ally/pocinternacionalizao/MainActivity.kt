2dc849769c73995aede47c935929824a28c9aae4
allyson <allyson.b.rodrigues@gmail.com>
2020-03-10 23:03:21 -0300

    Adicionando variáveis as strings

--- app/src/main/java/br/com/ally/pocinternacionalizao/MainActivity.kt
+++ app/src/main/java/br/com/ally/pocinternacionalizao/MainActivity.kt
@@ -1,12 +1,15 @@
 package br.com.ally.pocinternacionalizao
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        variableText.text = getString(R.string.variable_text, "Allyson", 33)
     }
+
 }
